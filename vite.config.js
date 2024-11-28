b470658b60e7b8046171acc2612abf421354ab95
Hu-Sa <54836979+Hu-Sa@users.noreply.github.com>
2024-11-28 16:54:33 +0100

    path & packages update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { optimizeImports, icons, optimizeCss } from "carbon-preprocess-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
   plugins: [
     svelte({ preprocess: [optimizeImports(), icons(), optimizeCss()] })
   ]
